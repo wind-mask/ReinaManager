5023478c54271cc77b0aaa85f5d32995b3098295
wind_mask <wind_mask_proton@proton.me>
2025-11-15 17:55:39 +0800

    fix: 不再使用tauri自带的更新；在linux优先自行构建或使用之后可能支持的包管理器

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,7 +19,6 @@ use crate::utils::{launch::stop_game, scan::scan_game_library};
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_window_state::Builder::new().build())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let window = app.get_webview_window("main").expect("no main window");
             let _ = window.show();
