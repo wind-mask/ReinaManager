7a6747c8d8a2230bb8b558adf2212033f34c2267
wind_mask <wind_mask_proton@proton.me>
2026-01-29 00:36:04 +0800

    🔧 config(config): cz pre-commit gitignore cliff readme

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -22,7 +22,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             if let Some(window) = app.get_webview_window("main") {
