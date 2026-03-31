a65ef6ea0212c5ba918d76fed7093f6610ed17ee
wind_mask <wind_mask_proton@proton.me>
2026-04-01 00:50:53 +0800

    ⬆️ dep-bump(dep): 更新依赖
    文件选择支持linux上的xdg-portal

--- src/pages/Detail/GameInfoEdit.tsx
+++ src/pages/Detail/GameInfoEdit.tsx
@@ -22,7 +22,7 @@ import {
 	TextField,
 	Typography,
 } from "@mui/material";
-import { basename, dirname } from "pathe";
+import { basename } from "pathe";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useImagePreview } from "@/hooks/common/useImagePreview";
@@ -236,9 +236,7 @@ export const GameInfoEdit: React.FC<GameInfoEditProps> = ({
 	// 处理选择可执行文件路径
 	const handleSelectLocalPath = async () => {
 		try {
-			const selectedPath = await handleExeFile(
-				localPath ? dirname(localPath) : "",
-			);
+			const selectedPath = await handleExeFile(localPath);
 			if (selectedPath) {
 				setLocalPath(selectedPath);
 			}
