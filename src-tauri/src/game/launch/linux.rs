9989ee4f9b5acddfda7e4f7e1dd8e3f9acb14551
wind_mask <wind_mask_proton@proton.me>
2026-06-21 18:24:21 +0800

    fix(linux): linux上的xdg-portal dialog

--- src-tauri/src/game/launch/linux.rs
+++ src-tauri/src/game/launch/linux.rs
@@ -1,6 +1,7 @@
 use crate::database::repository::games_repository::GamesRepository;
 use crate::game::monitor::TimeTrackingMode;
-use crate::game::monitor::{get_connection, get_manager_proxy, monitor_game, stop_game_session};
+use crate::game::monitor::{get_connection, get_manager_proxy};
+use crate::game::monitor::{monitor_game, stop_game_session};
 use log::{debug, info};
 use sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
